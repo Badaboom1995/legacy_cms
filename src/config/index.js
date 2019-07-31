3f1ab71e6b8a52cda0fc8ca95567b4ed179853b8
Alexey <belov.alexey.95@gmail.com>
2019-07-31 13:37:34 +0300

    add env

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,7 @@ import development from './development.config';
 import production from './production.config';
 
 const env = process.env.NODE_ENV || 'development';
+console.log('processEnv: ', process.env.NODE_ENV);
 
 const config = {
   development,
