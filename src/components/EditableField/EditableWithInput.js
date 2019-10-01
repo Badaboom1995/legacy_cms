9d3eacd49ff30d5e8add303cf57a97d7eb534a97
Alexey <belov.alexey.95@gmail.com>
2019-10-02 01:33:29 +0300

    edit task

--- src/components/EditableField/EditableWithInput.js
+++ src/components/EditableField/EditableWithInput.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './editable-element.scss';
 import Tasks from 'helpers/Tasks';
+import './editable-element.scss';
+import TextInput from 'components/TextInput/TextInput';
 
 class EditableWithInput extends React.Component {
   state = {
@@ -9,39 +11,28 @@ class EditableWithInput extends React.Component {
     value: '',
   };
   editingOn = () => {
-    this.setState(
-      () => ({ editing: true }),
-      () => {
-        this.text.focus();
-      },
-    );
+    this.setState(() => ({ editing: true }));
   };
   editingOff = () => {
     this.setState(() => ({ editing: false }));
     this.props.handleFunction(this.props.task.id, { [this.props.param_name]: this.state.value });
   };
-  onValueChange = () => {
-    this.setState(() => ({ value: this.text.value }));
+  onValueChange = value => {
+    this.setState(() => ({ value: value }));
   };
   render() {
     const { className } = this.props;
-    console.log(this.props.children);
+    const value = this.state.value || this.props.children;
     return (
       <React.Fragment>
         {this.state.editing ? (
           <div className="editable-element">
-            <input
-              className="editableElement__input"
-              type="text"
-              value={this.state.value || this.props.children}
-              ref={text => (this.text = text)}
-              onChange={this.onValueChange}
-            />{' '}
+            <TextInput name="answerToAdd" value={value} onChange={this.onValueChange} mathMode />
             <button onClick={this.editingOff}>submit</button>
           </div>
         ) : (
           <span onClick={this.editingOn} className={className}>
-            {this.state.value || this.props.children}
+            {value}
           </span>
         )}
       </React.Fragment>
